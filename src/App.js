8a85273cd3a90fa8f50d4ddb058f79de22884355
John Duncan <john@johnduncanphoto.com>
2018-01-22 20:04:22 +0000

    initial file structure defined

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import HomePage from "./containers/homepage.js";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <HomePage />;
+  }
+}
+
+export default App;
---
+++ src/containers/homepage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Web Gl</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
